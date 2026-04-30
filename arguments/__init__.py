2a6fcf842c8da94bcd8d32884fcae628bfcc7d7f
NagasaiManda <138184417+NagasaiManda@users.noreply.github.com>
2026-04-30 10:38:01 +0530

    d

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -96,8 +96,8 @@ class OptimizationParams(ParamGroup):
         self.exposure_lr_final = 0.001
         self.exposure_lr_delay_steps = 0
         self.exposure_lr_delay_mult = 0.0
-        self.percent_dense = 0.01
-        # self.percent_dense = 0.02
+        # self.percent_dense = 0.01
+        self.percent_dense = 0.005
         
         self.lambda_dssim = 0.2
         self.densification_interval = 100
@@ -108,11 +108,11 @@ class OptimizationParams(ParamGroup):
         
         
         self.densify_from_iter = 500
-        self.densify_until_iter = 15_000
-        # self.densify_until_iter = 18_000
+        # self.densify_until_iter = 15_000
+        self.densify_until_iter = 7_000
         
-        self.densify_grad_threshold = 0.0002
-        # self.densify_grad_threshold = 0.00035
+        # self.densify_grad_threshold = 0.0002
+        self.densify_grad_threshold = 0.001
         
         self.depth_l1_weight_init = 1.0
         # self.depth_l1_weight_init = 0.1
